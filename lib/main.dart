ee802d419bc3d6405092d0f437b8096f651201fb
Lendra <lendradzr77@gmail.com>
2026-02-17 21:21:04 +0700

    edit child:text

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  final ValueNotifier<int> counter = ValueNotifier(1);
   @override
   void initState() {
     super.initState();
@@ -39,13 +40,18 @@ class _GameScreenState extends State<GameScreen> {
                 borderRadius: BorderRadius.circular(10),
               ),
 
-              child: const Text(
-                'Score: 1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: ValueListenableBuilder<int>(
+                valueListenable: counter,
+                builder: (context, score, child) {
+                  return Text(
+                    'Score: $score',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
               ),
             ), //Container
           ), // Positioned
@@ -58,14 +64,14 @@ class _GameScreenState extends State<GameScreen> {
                 IconButton(
                   icon: const Icon(Icons.music_note, color: Colors.black),
                   onPressed: () {},
-                  ),
-                  IconButton(
+                ),
+                IconButton(
                   icon: const Icon(Icons.volume_up, color: Colors.black),
                   onPressed: () {},
-                  ),
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
